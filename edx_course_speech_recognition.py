2ed7d5e0bbdeb6bedc17b2e6e21e7ff3336aac46
Fayepasvouri <45490922+Fayepasvouri@users.noreply.github.com>
2020-05-13 15:24:55 +0100

    Update edx_course_speech_recognition.py

--- edx_course_speech_recognition.py
+++ edx_course_speech_recognition.py
@@ -143,9 +143,27 @@ class InversePreEmphasis(torch.nn.Module):
     # compute the mean vector of fbank coefficients in the utterance and subtract it from all frames of fbank coefficients
     def mean_norm_fbank(self, fbank):
         self.flanky=self.flank
-        # compute mean fbank vector of all frames
-        # subtract it from each frame
-        return fbank
+        low_freq_mel = 0
+        high_freq_mel = (2595 * numpy.log10(1 + (sample_rate / 2) / 700))  # Convert Hz to Mel
+        mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+        hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+        bin = numpy.floor((NFFT + 1) * hz_points / sample_rate)
+
+        fbank = numpy.zeros((nfilt, int(numpy.floor(NFFT / 2 + 1))))
+        for m in range(1, nfilt + 1):
+           f_m_minus = int(bin[m - 1])   # left
+           f_m = int(bin[m])             # center
+           f_m_plus = int(bin[m + 1])    # right
+
+        for k in range(f_m_minus, f_m):
+           fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+           fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+           filter_banks = numpy.dot(pow_frames, fbank.T)
+           filter_banks = numpy.where(filter_banks == 0, numpy.finfo(float).eps, filter_banks)  # Numerical Stability
+           filter_banks = 20 * numpy.log10(filter_banks)
+           fbank=filter_banks
+           return fbank
 
  
     def accumulate_stats(self, fbank):
